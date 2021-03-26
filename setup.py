105cba559d79d67b821d485d517b95a98659ab7a
Mason Watson <msn.watson@gmail.com>
2021-03-26 14:21:23 -0400

    remove packages from requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2' 
+VERSION = '0.0.1.2' 
 DESCRIPTION = 'Imp. of EM Clustering Algorithm'
 LONG_DESCRIPTION = 'Implementation of the EM Clustering Algorithm for arbitrary models'
 
@@ -18,9 +18,6 @@ setup(
             'ticks',
             'scipy',
             'numpy',
-            'copy',
-            'abc',
-            'collections',
             'matplotlib'
         ],
         
